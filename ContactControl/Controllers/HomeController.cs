6cef7cff68701baa456338308f34b7961e459742
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-03-25 21:59:47 -0300

    Initial project settings

--- ContactControl/Controllers/HomeController.cs
+++ ContactControl/Controllers/HomeController.cs
@@ -11,12 +11,6 @@ namespace ContactControl.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
